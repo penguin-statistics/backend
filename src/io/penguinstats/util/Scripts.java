ee4ffddfc5b08c829b2908d759ede54c019c29a6
AlvISsReimu <alvissreimu@gmail.com>
2019-06-17 12:06:48 -0700

    remove some dangerous methods

--- src/io/penguinstats/util/Scripts.java
+++ src/io/penguinstats/util/Scripts.java
@@ -3,63 +3,23 @@ package io.penguinstats.util;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.Document;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import io.penguinstats.bean.Drop;
 import io.penguinstats.bean.DropMatrix;
 import io.penguinstats.bean.ItemDrop;
-import io.penguinstats.dao.BaseDao;
-import io.penguinstats.dao.ItemDropDao;
 import io.penguinstats.service.DropMatrixService;
 import io.penguinstats.service.ItemDropService;
 
 public class Scripts {
 
-	private static final ItemDropDao itemDropDao = new ItemDropDao();
 	private static final DropMatrixService dropMatrixService = DropMatrixService.getInstance();
 	private static final ItemDropService itemDropService = ItemDropService.getInstance();
 
 	public static void main(String[] args) {
 		outputAllReliableItemDrops();
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void convertOldItemDrop() {
-		Dao oldDao = new Dao("item_drop");
-		List<Document> documents = oldDao.findAllDocuments();
-		List<ItemDrop> itemDrops = new ArrayList<>();
-		for (Document document : documents) {
-			String stageId = ConvertHelper.stageIdMap.get(document.getInteger("stageID"));
-			Integer times = document.getInteger("times");
-			Long timestamp = document.getLong("timestamp");
-			String ip = document.getString("ip");
-			Boolean isReliable = document.getBoolean("isAbnormal") == null ? true : !document.getBoolean("isAbnormal");
-			List<Document> dropsDocList = (ArrayList<Document>)document.get("drops");
-			List<Drop> drops = new ArrayList<>();
-			dropsDocList.forEach(dropDoc -> {
-				String itemId = ConvertHelper.itemIdMap.get(dropDoc.getInteger("itemID"));
-				if (itemId != null) {
-					drops.add(new Drop(itemId, dropDoc.getInteger("quantity")));
-				}
-			});
-			Integer furnitureNum = document.getInteger("furnitureNum");
-			if (furnitureNum != null && furnitureNum > 0) {
-				drops.add(new Drop("furni", furnitureNum));
-			}
-			if (stageId != null && times != null && timestamp != null && ip != null) {
-				ItemDrop itemDrop = new ItemDrop(stageId, times, drops, timestamp, ip, isReliable);
-				itemDrops.add(itemDrop);
-				System.out.println(itemDrop.asJSON().toString());
-			}
-		}
-		if (itemDropDao.batchDelete()) { // WARNING: Check table name!
-			itemDropDao.batchSave(itemDrops);
-		}
 	}
 
 	private static void clearAndUpdateDropMatrix() {
@@ -82,15 +42,6 @@ public class Scripts {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	@SuppressWarnings("rawtypes")
-	private static class Dao extends BaseDao {
-
-		public Dao(String collectionName) {
-			super(collectionName);
-		}
-
 	}
 
 }
