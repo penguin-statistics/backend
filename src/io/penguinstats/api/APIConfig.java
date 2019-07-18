124d197479873a4d04dcec9157d7bf5e4ea378a3
AlvISsReimu <alvissreimu@gmail.com>
2019-07-18 03:48:16 -0700

    no need to add no-store header any more

--- src/io/penguinstats/api/APIConfig.java
+++ src/io/penguinstats/api/APIConfig.java
@@ -5,7 +5,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 import io.penguinstats.api.filter.CORSFilter;
 import io.penguinstats.api.filter.ReadUserIDCookieFilter;
 import io.penguinstats.api.filter.ReadUserIDCookieOrCreateFilter;
-import io.penguinstats.api.filter.SetNoStoreHeaderFilter;
 import io.penguinstats.api.filter.SetUserIDCookieFilter;
 
 public class APIConfig extends ResourceConfig {
@@ -14,7 +13,6 @@ public class APIConfig extends ResourceConfig {
 		this.register(ReadUserIDCookieFilter.class).register(ReadUserIDCookieOrCreateFilter.class)
 				.register(SetUserIDCookieFilter.class);
 		this.register(CORSFilter.class);
-		this.register(SetNoStoreHeaderFilter.class);
 		this.packages("io.penguinstats.api");
 		System.out.println("APIConfig finished");
 	}
