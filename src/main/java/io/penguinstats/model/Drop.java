16029876d6c6094f047a29cc34a82c8c7f2aa571
YukiC <864794490@qq.com>
2019-09-26 12:49:57 +0800

    Update Drop.java
    fix typo

--- src/main/java/io/penguinstats/model/Drop.java
+++ src/main/java/io/penguinstats/model/Drop.java
@@ -1,5 +1,6 @@
 package io.penguinstats.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
