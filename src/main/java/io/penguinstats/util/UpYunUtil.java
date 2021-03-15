1a8201a347ab4e30a2eb6c14d4144e7d0d1d0177
AlvISsReimu <alvissreimu@gmail.com>
2021-03-16 02:33:20 +0800

    add utils for upyun

---
+++ src/main/java/io/penguinstats/util/UpYunUtil.java
@@ -0,0 +1,117 @@
+package io.penguinstats.util;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
+
+import io.penguinstats.enums.ErrorCode;
+import io.penguinstats.util.exception.ServiceException;
+
+public class UpYunUtil {
+
+    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+
+    private UpYunUtil() {}
+
+    /**
+     * Get 'Authorization' for UpYun file uploading
+     *
+     * @param method Should be POST
+     * @param dateTime The timestamp of the request. Must be consistent with the one in the policy. Optional
+     * @param path /&lt;bucket&gt;
+     * @param policy
+     * @param userName Operator's name
+     * @param password
+     * @param md5 Optional
+     * @return
+     */
+    public static String getAuthorization(String method, Long dateTime, String path, String policy, String userName,
+            String password, String md5) {
+        StringBuilder sb = new StringBuilder();
+        String sp = "&";
+
+        sb.append(method);
+
+        sb.append(sp);
+        sb.append(path);
+
+        if (dateTime != null) {
+            String date = getDateTimeRFC1123String(dateTime);
+            sb.append(sp);
+            sb.append(date);
+        }
+
+        if (!StringUtils.isEmpty(policy)) {
+            sb.append(sp);
+            sb.append(policy);
+        }
+
+        if (!StringUtils.isEmpty(md5)) {
+            sb.append(sp);
+            sb.append(md5);
+        }
+
+        String raw = sb.toString().trim();
+        byte[] hmac = null;
+        try {
+            hmac = calculateRFC2104HMACRaw(DigestUtils.md5Hex(password).toLowerCase(), raw);
+        } catch (Exception e) {
+            throw new ServiceException(ErrorCode.INTERNAL_SERVER_ERROR, "calculate SHA1 wrong.");
+        }
+
+        if (hmac != null) {
+            return "UPYUN " + userName + ":" + Base64.getEncoder().encodeToString(hmac);
+        }
+
+        return null;
+    }
+
+    /**
+     * Calculate policy
+     *
+     * @param bucket
+     * @param saveKey The path of the saved file. Example: /upload_{random32}{.suffix}
+     * @param expiration The expiration of the request. UNIX UTC timestamp. Unit is second.
+     * @return
+     */
+    public static String getPolicy(String bucket, String saveKey, Long expiration) {
+        Map<String, Object> policyMap = new HashMap<>();
+        policyMap.put("bucket", bucket);
+        policyMap.put("save-key", saveKey);
+        policyMap.put("expiration", expiration);
+        JSONObject obj = new JSONObject(policyMap);
+        return Base64.getEncoder().encodeToString(obj.toString().getBytes());
+    }
+
+    private static String getDateTimeRFC1123String(Long dateTime) {
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+        DateTimeFormatter formatter = DateTimeFormatter.RFC_1123_DATE_TIME;
+        return now.format(formatter);
+    }
+
+    private static byte[] calculateRFC2104HMACRaw(String key, String data)
+            throws SignatureException, NoSuchAlgorithmException, InvalidKeyException {
+        byte[] keyBytes = key.getBytes();
+        SecretKeySpec signingKey = new SecretKeySpec(keyBytes, HMAC_SHA1_ALGORITHM);
+
+        // Get an hmac_sha1 Mac instance and initialize with the signing key
+        Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+        mac.init(signingKey);
+        // Compute the hmac on input data bytes
+        return mac.doFinal(data.getBytes());
+    }
+
+}
