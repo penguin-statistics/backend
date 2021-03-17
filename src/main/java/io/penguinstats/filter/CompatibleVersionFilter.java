9bf026a714ff67d0aa842aca6a0c00b37d075bf3
AlvISsReimu <alvissreimu@gmail.com>
2021-03-18 02:35:15 +0800

    solve AbstractMethodError in filter

--- src/main/java/io/penguinstats/filter/CompatibleVersionFilter.java
+++ src/main/java/io/penguinstats/filter/CompatibleVersionFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,12 +16,18 @@ import io.penguinstats.constant.Constant;
 @WebFilter(urlPatterns = {"/api/v2/*"})
 public class CompatibleVersionFilter implements Filter {
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		HttpServletResponse res = (HttpServletResponse)response;
-		res.setHeader(Constant.CustomHeader.X_PENGUIN_COMPATIBLE, Constant.CompatibleVersion.FRONTEND_V2_3_4_0);
-		chain.doFilter(request, response);
-	}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse)response;
+        res.setHeader(Constant.CustomHeader.X_PENGUIN_COMPATIBLE, Constant.CompatibleVersion.FRONTEND_V2_3_4_0);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
 
 }
