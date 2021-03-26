bc203ff5f1df8253187b5b0060ccbd722313ff21
AlvISsReimu <alvissreimu@gmail.com>
2021-03-27 03:14:59 +0800

    update version to 2.4.0

--- src/main/java/io/penguinstats/configuration/SwaggerConfig.java
+++ src/main/java/io/penguinstats/configuration/SwaggerConfig.java
@@ -23,40 +23,40 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
 
-	@Bean
-	public Docket backend() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(apis()).paths(paths()).build();
-	}
+    @Bean
+    public Docket backend() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(apis()).paths(paths()).build();
+    }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Penguin Statistics - REST APIs").description(
-				"Backend APIs for Arknights drop rate statistics website 'Penguin Statistics': https://penguin-stats.io/")
-				.contact(new Contact("AlvISs_Reimu", "https://github.com/AlvISsReimu", "alvissreimu@gmail.com"))
-				.license("MIT License").licenseUrl("https://github.com/penguin-statistics/backend/blob/master/LICENSE")
-				.version("2.3.0").build();
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Penguin Statistics - REST APIs").description(
+                "Backend APIs for Arknights drop rate statistics website 'Penguin Statistics': https://penguin-stats.io/")
+                .contact(new Contact("AlvISs_Reimu", "https://github.com/AlvISsReimu", "alvissreimu@gmail.com"))
+                .license("MIT License").licenseUrl("https://github.com/penguin-statistics/backend/blob/master/LICENSE")
+                .version("2.4.0").build();
+    }
 
-	private Predicate<RequestHandler> apis() {
-		return RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class);
-	}
+    private Predicate<RequestHandler> apis() {
+        return RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class);
+    }
 
-	private Predicate<String> paths() {
-		return PathSelectors.any();
-	}
+    private Predicate<String> paths() {
+        return PathSelectors.any();
+    }
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/META-INF/resources/");
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/META-INF/resources/");
+    }
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/swagger/v2/api-docs", "/v2/api-docs");
-		registry.addRedirectViewController("/swagger/swagger-resources/configuration/ui",
-				"/swagger-resources/configuration/ui");
-		registry.addRedirectViewController("/swagger/swagger-resources/configuration/security",
-				"/swagger-resources/configuration/security");
-		registry.addRedirectViewController("/swagger/swagger-resources", "/swagger-resources");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/swagger/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/swagger/swagger-resources/configuration/ui",
+                "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/swagger/swagger-resources/configuration/security",
+                "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/swagger/swagger-resources", "/swagger-resources");
+    }
 
 }
